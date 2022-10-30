19cdce656962b586e0c23ade3b3cada852497619
Serena D <98000871+jsxNeko@users.noreply.github.com>
2022-10-29 23:03:31 -0500

    Update Links

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 	return (
 		<div className='footer'>
 			<p className="text-uppercase black link">© <a 
-				href='https://github.com/serenuy/facial-recognition-app'
+				href='https://github.com/jsxNeko/facial-recognition-app'
 				style={{'textDecoration':'none'}}>
 				Serenuy/@github</a> 
 			<script>document.write(new Date().getFullYear())</script>
